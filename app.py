cc8b3df24bdd4e41b4348dcf7180b8e253cb29e0
mikeleo03 <leonmichael463@gmail.com>
2024-01-05 17:50:13 +0700

    feat : initiate connection to database

--- app.py
+++ app.py
@@ -1,7 +1,35 @@
 from flask import Flask, render_template, Response
 from lib.framegenerator import generate_frames
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
+
+# Read environment variable
+load_dotenv()
+variable_name = 'MONGO_URI'
+uri = os.environ[variable_name]
+
+if uri:
+    print(f"The value of {variable_name} is: {uri}")
+else:
+    print(f"{variable_name} is not set.")
+    
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Send a ping to confirm a successful connection
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
+
+# Set up database and collection
+db = client.xhole
+collection = db.data_train
 
 @app.route('/')
 def home():
@@ -15,6 +43,10 @@ def detector():
 @app.route('/video_feed')
 def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+@app.route('/recommend')
+def recommend():
+    return render_template('recommend.html', title='XHole Detection Recommendation')
 
 if __name__ == '__main__':
     app.run(debug=True)
