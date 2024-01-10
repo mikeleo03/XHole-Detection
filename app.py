169a6dbe03c3fa194001b7b5f98a71e39e2c6d91
mikeleo03 <leonmichael463@gmail.com>
2024-01-10 14:40:18 +0700

    feat : gather appropriate data from the form

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template, request, Response
 from lib.framegenerator import generate_frames
+from lib.algorithm.Rock_Factor import Rock_Factor
+from lib.algorithm.KuzRam_Fragmentation import KuzRam_Fragmentation
+from lib.algorithm.Rosin_Rammler import Rosin_Rammler
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
@@ -29,7 +32,7 @@ except Exception as e:
 
 # Set up database and collection
 db = client.xhole
-collection = db.data_train
+stone_data = db.stone
 
 def get_rock_options():
     # Replace this with your actual MongoDB query logic
@@ -55,6 +58,69 @@ def video_feed():
 def recommend():
     rock_options = get_rock_options()
     return render_template('recommend.html', title='XHole Detection Recommendation', rock_options=rock_options)
+
+@app.route('/result', methods=['POST'])
+def submit_form():
+    if request.method == 'POST':
+        # Access form data
+        # 0. Setup rock and explosives
+        rock_type = request.form['rock']
+        # Ambil 3 constant utamanya
+        specific_gravity = 2.9
+        hardness = 6.09
+        rock_density = 181.0412
+        
+        explosive_type = request.form['explosives']
+        blasting_energy = 0
+        blasthole_diameter = 0
+        if explosive_type == "TNT":
+            blasting_energy = 115
+            blasthole_diameter = 0.005
+        elif explosive_type == "ANFO":
+            blasting_energy = 100
+            blasthole_diameter = 0.03
+            
+        explosives_density = request.form['expdensity']
+        detonation_speed = request.form['detspeed']
+        
+        # 1. Rock Factor
+        rock_mass_description = request.form['rmd']
+        joint_plane_spacing = request.form['jps']
+        joint_plane_orientation = request.form['jpo']
+        
+        # Calculating the rock factor
+        rock_factor_class = Rock_Factor(rock_mass_description, joint_plane_spacing, joint_plane_orientation, specific_gravity, hardness)
+        rock_factor = rock_factor_class.run()
+        print("Rock factor:", rock_factor)
+        
+        # 2. Kuz Ram Fragmentation
+        high_level = request.form['level']
+        ignition_method = request.form['ignition']
+        
+        rock_deposition = request.form['rockdepo']
+        geologic_structure = request.form['geostruc']
+        number_of_rows = request.form['rows']  
+        
+        # Calculating the fragmentation size
+        kuzram_class = KuzRam_Fragmentation(explosives_density, detonation_speed, blasting_energy, rock_density, blasthole_diameter, high_level)
+        fragmentation_size = kuzram_class.run(rock_factor, rock_deposition, geologic_structure, number_of_rows, ignition_method)
+        print("Fragmentation size:", fragmentation_size)
+        
+        # 3. Rosin-Rammler Calculations
+        stdev_drilling_accuracy = request.form['stdevdrill']
+        corrected_burden = kuzram_class.get_corrected_burden()
+        stiffness = kuzram_class.get_stiffness()
+        print("Corrected Burden:", corrected_burden)
+        rossin_rammler_class = Rosin_Rammler(stdev_drilling_accuracy, corrected_burden, fragmentation_size, blasthole_diameter, high_level)
+        rossin_rammler_class.run(stiffness)
+
+        # Perform any necessary processing, for example, store in MongoDB or run calculations
+
+        # Return a response or redirect to another page
+        return render_template('result.html', result="Form submitted successfully!")
+
+    # Handle other HTTP methods or redirect to form page
+    return render_template('form_page.html')
 
 # Examples of basic CRUD methods
 @app.route("/read")
